f7d01d0c2914c51323d79141e701e5be47213abd
玲思 <lijun.jlj@alibaba-inc.com>
2015-07-29 16:01:29 +0800

    重构了ViewController

---
+++ ImageDownload/View/DownLoadTableView.h
@@ -0,0 +1,13 @@
+//
+//  DownLoadTableView.h
+//  ImageDownload
+//
+//  Created by justinjing on 15/7/29.
+//  Copyright (c) 2015年 justinjing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DownLoadTableView : UIView
+
+@end
--- ImageDownload/View/DownLoadTableView.m
+++ ImageDownload/View/DownLoadTableView.m
@@ -1,13 +1,12 @@
 //
-//  ViewController.m
+//  DownLoadTableView.m
 //  ImageDownload
 //
 //  Created by justinjing on 15/7/29.
 //  Copyright (c) 2015年 justinjing. All rights reserved.
 //
 
-#import "ViewController.h"
-
+#import "DownLoadTableView.h"
 #import "LWConnectionManager.h"
 #import "DownLoadDataSource.h"
 #import "DownLoadTableViewCell.h"
@@ -16,9 +15,9 @@
 #define DL_SCREEN_HEIGHT ((float)[[UIScreen mainScreen] bounds].size.height)
 #define DL_SCREEN_WIDTH  ((float)[[UIScreen mainScreen] bounds].size.width)
 
-@interface ViewController () <UITableViewDelegate,LWConnectionDelegate>
+@interface DownLoadTableView () <UITableViewDelegate,LWConnectionDelegate>
 {
-     NSArray * urlArr;
+    NSArray * urlArr;
 }
 
 @property(nonatomic ,strong)UITableView *rightTableView;
@@ -26,34 +25,39 @@
 @end
 
 
-@implementation ViewController
+@implementation DownLoadTableView
 
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    self.title = @"DownLoad";
-    self.view.backgroundColor =[UIColor whiteColor];
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        [self initTheView];
+    }
+    return self;
+}
+
+- (void)initTheView {
     
-    urlArr = [[NSArray alloc] initWithObjects:@"http://www.wallcoo.com/nature/Japan_Hokkaido_Furano_Country_field_1920x1200/wallpapers/1920x1080/Japan-Hokkaido-Landscape-WUXGA_country_field_0149.jpg",
-              @"http://d.hiphotos.baidu.com/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=487e85ccd158ccbf0fb1bd6878b1d75b/8ad4b31c8701a18b7c763e319e2f07082838fe2b.jpg",
-              @"http://c.hiphotos.baidu.com/baike/c0%3Dbaike92%2C5%2C5%2C92%2C30/sign=89d10008ddc451dae2fb04b9d7943903/08f790529822720e9ef77aac7bcb0a46f21fabaf.jpg",
-              @"http://f.hiphotos.baidu.com/baike/c0%3Dbaike72%2C5%2C5%2C72%2C24/sign=7616f810d40735fa85fd46ebff3864d6/0d338744ebf81a4c8c01b6c2d72a6059252da68e.jpg",nil];
-    
-    self.dataSource = [[DownLoadDataSource alloc] init];
-    self.dataSource.rowData = urlArr;
-    
-    self.rightTableView=[[UITableView alloc]initWithFrame:CGRectMake(0,0, DL_SCREEN_WIDTH,DL_SCREEN_HEIGHT) style:UITableViewStylePlain];
-    self.rightTableView.backgroundColor = [UIColor clearColor];
-    self.rightTableView.scrollsToTop = YES;
-    self.rightTableView.dataSource = self.dataSource;
-    self.rightTableView.delegate = self;
-    
-    self.rightTableView.showsVerticalScrollIndicator=NO;
-    self.rightTableView.tableFooterView = [UIView new];
-    [self.view addSubview:self.rightTableView];
-    
-    [self startDownload];
+        urlArr = [[NSArray alloc] initWithObjects:@"http://www.wallcoo.com/nature/Japan_Hokkaido_Furano_Country_field_1920x1200/wallpapers/1920x1080/Japan-Hokkaido-Landscape-WUXGA_country_field_0149.jpg",
+                  @"http://d.hiphotos.baidu.com/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=487e85ccd158ccbf0fb1bd6878b1d75b/8ad4b31c8701a18b7c763e319e2f07082838fe2b.jpg",
+                  @"http://c.hiphotos.baidu.com/baike/c0%3Dbaike92%2C5%2C5%2C92%2C30/sign=89d10008ddc451dae2fb04b9d7943903/08f790529822720e9ef77aac7bcb0a46f21fabaf.jpg",
+                  @"http://f.hiphotos.baidu.com/baike/c0%3Dbaike72%2C5%2C5%2C72%2C24/sign=7616f810d40735fa85fd46ebff3864d6/0d338744ebf81a4c8c01b6c2d72a6059252da68e.jpg",nil];
+
+        self.dataSource = [[DownLoadDataSource alloc] init];
+        self.dataSource.rowData = urlArr;
+
+        self.rightTableView=[[UITableView alloc]initWithFrame:CGRectMake(0,0,DL_SCREEN_WIDTH,DL_SCREEN_HEIGHT) style:UITableViewStylePlain];
+        self.rightTableView.backgroundColor = [UIColor clearColor];
+        self.rightTableView.scrollsToTop = YES;
+        self.rightTableView.dataSource = self.dataSource;
+        self.rightTableView.delegate = self;
+
+        self.rightTableView.showsVerticalScrollIndicator=NO;
+        self.rightTableView.tableFooterView = [UIView new];
+        [self addSubview:self.rightTableView];
+
+        [self startDownload];
 }
 
 
@@ -122,4 +126,5 @@
         
     }
 }
+
 @end
